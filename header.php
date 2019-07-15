ac03b164897903e968f584f1cb349019f644166a
Alexis Rousseau <alexis@MacBook-Pro-de-Alexis.local>
2019-07-15 19:31:03 +0200

    modify header style

--- header.php
+++ header.php
@@ -1,11 +1,6 @@
 <div class="mask"></div>
     <div id="header" class="col-xs-14">
-        <div id="logo" class="col-xs-8 col-xs-offset-2 col-sm-5 col-md-3 col-md-offset-3">
-            <a href="<?php echo get_bloginfo( 'wpurl' );?>"><img src="<?php bloginfo('template_url') ?>/assets/img/logo_2.svg" alt="93degres"/></a>
-        </div>
-        <div id="mobile-menu" class="mobile-menu">
-            <span></span>
-        </div>
+        <div class="col-xs-5 col-xs-offset-1">
             <?php 
             $args = array(
             'depth'       => 0,
@@ -19,12 +14,18 @@
             'link_after'  => ''
             );
             wp_nav_menu( $args ); ?>
+        </div>
+        <div id="logo" class="col-xs-2">
+            <a href="<?php echo get_bloginfo( 'wpurl' );?>"><img src="<?php bloginfo('template_url') ?>/assets/img/logo_2.svg" alt="93degres"/></a>
+        </div>
+        <div id="menu-secondary-container" class="col-xs-5">
+            <div id="link--about"><a href="<?php echo get_bloginfo( 'wpurl' );?>/a-propos">À propos</a></div>
+            <div id="link--instagram"><a href="https://www.instagram.com/93.degres/" target="_blank"></a></div>
+        </div>
+        <div id="mobile-menu" class="mobile-menu">
+            <span></span>
+        </div>
 
 
     </div>
-     <div class="rotate">
-    <div class="scroll-down"></div>
-    <div id="link--about"><a href="<?php echo get_bloginfo( 'wpurl' );?>/a-propos">À propos</a></div>
-    <a id="link--instagram" href="https://www.instagram.com/93.degres/" target="_blank"></a>
-    <a id="link--twitter" href="https://twitter.com/93degres" target="_blank"></a>
-</div>
+ 
