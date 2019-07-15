c8fefcaf47e01433df764fb0f21292b79fa096cc
Alexis Rousseau <alexis@MacBook-Pro-de-Alexis.local>
2019-07-15 14:01:30 +0200

    add font to theme

--- header.php
+++ header.php
@@ -1,5 +1,5 @@
 <div class="mask"></div>
-    <div id="header" class="col-xs-48">
+    <div id="header" class="col-xs-14">
         <div id="logo" class="col-xs-8 col-xs-offset-2 col-sm-5 col-md-3 col-md-offset-3">
             <a href="<?php echo get_bloginfo( 'wpurl' );?>"><img src="<?php bloginfo('template_url') ?>/assets/img/logo_2.svg" alt="93degres"/></a>
         </div>
