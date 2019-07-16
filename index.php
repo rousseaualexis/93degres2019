ea8ea2a7b1ba2dfa1fa4c1b8fbf43fa60b945890
Alexis Rousseau <alexis@MacBook-Pro-de-Alexis.local>
2019-07-16 22:25:51 +0200

    add round circle on cover homepage

--- index.php
+++ index.php
@@ -7,8 +7,7 @@ Template Name: Homepage
 <body class="homepage">
 <?php get_header(); ?>
 <div class="container">
-
-    <div class="col-xs-48 col-xs-push-0 col-md-push-3">
+    <div id="homepage--cover" class="row">
         <?php
             $args = array(
                 'post_type' => array('articles'),
@@ -21,10 +20,8 @@ Template Name: Homepage
                     $count++;
                     if($count == 1) :
         ?>
-                    <?php if('articles' === get_post_type()): ?>
-                        <div class="first-post post post--guide col-xs-pull-0 col-xs-48 col-md-pull-12">
-                    <?php endif; ?>
-                            <div class="first-post-texte col-xs-42 col-xs-pull-0 col-xs-offset-3 col-sm-24 col-sm-push-6 col-md-push-16 col-md-offset-0">
+
+
                                 <?php
                                     $id = get_the_id();
                                     $terms = get_the_terms( $id, 'category' );
@@ -35,21 +32,20 @@ Template Name: Homepage
                                         $term_name = $term->name;
                                     }
                                 ?>
-                                <a class="categories" href="<?php echo $term_url; ?>"><img src="<?php echo $flag_url;?>" alt="<?php echo $flag['alt'];?>"/ ><div><?php echo $term_name; ?></div></a>
-                                <div id="title">
-                                <span class="h1 col-xs-10 col-xs-offset-1"><?php the_title(); ?></span><?php if(!empty(get_field('subtitle'))){echo '<p><strong>' . get_field('subtitle') . '</strong></p>';}?>
-                            	</div>
-                                <a class="a-cta col-xs-offset-0 col-sm-offset-13 col-md-offset-12" href="<?php the_permalink(); ?>"><div class="cta">Découvrir</div>
-                                </a>
+                                
+                                <span class="h1 col-xs-10 col-xs-offset-1"><?php the_title(); ?></span>
+                                <a class="categories col-xs-offset-1" href="<?php echo $term_url; ?>"><img src="<?php echo $flag_url;?>" alt="<?php echo $flag['alt'];?>"/ ><div><?php echo $term_name; ?></div></a>
+                                <div class="cta--circle col-xs-offset-1">
+                                    <a class="" href="<?php the_permalink(); ?>"><div>Découvrir</div>
+                                    </a>
+                                </div>
                               
-
-                            </div>
                             <div class="first-post-image col-xs-31 col-xs-offset-0 col-md-offset-23">
                                 <div class="image" style="background-image: url('<?php echo $thumbnail_url;?>');" title="<?php echo $thumbnail['alt']; ?>">
                                 </div>
                             </div>
 
-                        </div>
+                
                         <div class="grid scroll-reveal col-md-46 col-md-pull-1">
         <?php
                             else :
