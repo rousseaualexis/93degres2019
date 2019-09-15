6fd9644177d895480f4c204852d638d0cf77461c
Alexis <alexis@MacBook-Pro-de-Alexis.local>
2019-09-15 20:27:14 +0200

    normalize meyer

--- assets/views/template-parts/block/content-title.php
+++ assets/views/template-parts/block/content-title.php
@@ -9,7 +9,7 @@
 $title = strip_tags(get_field('title'), '<br><em><strong>');
 $small_description = strip_tags(get_field('small-description'), '<br><em><strong>');
 ?>
-<div class="acf--title content">
+<div class="acf--title content row">
 	<div class="col-sm-11 col-sm-offset-9">
 		<h2 class="h3"><?php echo $title; ?></h2>
 	</div>
